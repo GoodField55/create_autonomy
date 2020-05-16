5a4910eb483f29bdce50cd515a0e7bce3b8e25dd
GoodField55 <goodfield055@gmail.com>
2020-05-16 01:38:08 +0000

    Remove build directory

--- ca_driver/build/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include;/home/ubuntu/catkin_ws/devel/include;/usr/include".split(';') if "${prefix}/include;/home/ubuntu/catkin_ws/devel/include;/usr/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "ca_msgs;diagnostic_msgs;diagnostic_updater;geometry_msgs;nav_msgs;roscpp;sensor_msgs;std_msgs;tf".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "/home/ubuntu/catkin_ws/devel/lib/libcreate.so;/usr/lib/arm-linux-gnueabihf/libboost_system.so;/usr/lib/arm-linux-gnueabihf/libboost_thread.so;/usr/lib/arm-linux-gnueabihf/libboost_chrono.so;/usr/lib/arm-linux-gnueabihf/libboost_date_time.so;/usr/lib/arm-linux-gnueabihf/libboost_atomic.so;/usr/lib/arm-linux-gnueabihf/libpthread.so".split(';') if "/home/ubuntu/catkin_ws/devel/lib/libcreate.so;/usr/lib/arm-linux-gnueabihf/libboost_system.so;/usr/lib/arm-linux-gnueabihf/libboost_thread.so;/usr/lib/arm-linux-gnueabihf/libboost_chrono.so;/usr/lib/arm-linux-gnueabihf/libboost_date_time.so;/usr/lib/arm-linux-gnueabihf/libboost_atomic.so;/usr/lib/arm-linux-gnueabihf/libpthread.so" != "" else []
-PROJECT_NAME = "ca_driver"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "1.3.0"
